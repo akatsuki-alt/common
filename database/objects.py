3efb41afdab72d43cbbb046ec8f4bcf955f9c4fb
kanaarima <137962405+kanaarima@users.noreply.github.com>
2024-01-30 07:05:52 +0000

    Postgres boilerplate

---
+++ database/objects.py
@@ -0,0 +1,12 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+
+from datetime import datetime
+from typing import Optional
+
+from sqlalchemy.dialects.postgresql import JSONB, ARRAY
+from sqlalchemy import *
+
+
+Base = declarative_base()
