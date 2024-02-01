60c1c33906fb800fb631b5224b47199865b9fa6f
kanaarima <137962405+kanaarima@users.noreply.github.com>
2024-02-01 11:39:02 +0000

    Add beatmap repo

---
+++ repos/beatmaps.py
@@ -0,0 +1,95 @@
+from ..database.objects import DBBeatmapset, DBBeatmap
+from ossapi import Beatmap, Beatmapset
+from ..utils import OSSAPI_GAMEMODES
+from ..app import ossapi, database
+
+import logging
+
+logger = logging.getLogger("repos.beatmaps")
+
+def _get_username(user_id: int) -> str:
+    if (user := ossapi.user(user_id)):
+        return user.username
+    else:
+        return None
+
+def _from_api_beatmap(beatmap: Beatmap) -> DBBeatmap:
+    dbmap = DBBeatmap(
+        id=beatmap.id,
+        set_id=beatmap.beatmapset_id,
+        mode=OSSAPI_GAMEMODES[beatmap.mode.value],
+        md5=beatmap.checksum,
+        version=beatmap.version,
+        last_update=beatmap.last_updated,
+        max_combo=beatmap.max_combo,
+        bpm=beatmap.bpm,
+        cs=beatmap.cs,
+        od=beatmap.accuracy,
+        ar=beatmap.ar,
+        hp=beatmap.drain,
+        diff=beatmap.difficulty_rating,
+        total_length=beatmap.total_length,
+        hit_length=beatmap.hit_length,
+        count_circles=beatmap.count_circles,
+        count_sliders=beatmap.count_sliders,
+        count_spinners=beatmap.count_spinners,
+    )
+    # TODO: priv server status
+    dbmap.status = {
+        'bancho': beatmap.status.value,
+        'akatsuki': beatmap.status.value,
+        'titanic': -2
+    }
+    return dbmap
+
+def _from_api_beatmapset(beatmapset: Beatmapset) -> DBBeatmapset:
+    dbset = DBBeatmapset(
+        id=beatmapset.id,
+        artist=beatmapset.artist,
+        artist_unicode=beatmapset.artist_unicode,
+        title=beatmapset.title,
+        title_unicode=beatmapset.title_unicode,
+        source=beatmapset.source,
+        mapper=beatmapset.creator,
+        tags=beatmapset.tags.split(" "),
+        pack_tags=beatmapset.pack_tags,
+        nsfw=beatmapset.nsfw,
+        video=beatmapset.video,
+        spotlight=beatmapset.spotlight,
+        availability=not beatmapset.availability.download_disabled,
+        ranked_date=beatmapset.ranked_date,
+        submitted_date=beatmapset.submitted_date
+    )
+    if beatmapset.genre:
+        dbset.genre = beatmapset.genre['name']
+    if beatmapset.language:
+        dbset.language = beatmapset.language['name']
+    noms = []
+    if beatmapset.current_nominations:
+        for nom in beatmapset.current_nominations:
+            if (nom_user := _get_username(nom.user_id)):
+                noms.append(nom_user)
+    dbset.nominators = {
+        'bancho': noms,
+        'akatsuki': [],
+        'titanic': []
+    }
+    return dbset
+
+def get_beatmapset(beatmapset_id: int, force_fetch: bool = False) -> DBBeatmapset | None:
+    with database.session as session:
+        if (dbset := session.query(DBBeatmapset).filter(DBBeatmapset.id == beatmapset_id).first()) and not force_fetch:
+            return dbset
+        else:
+            try:
+                beatmapset = ossapi.beatmapset(beatmapset_id)
+                dbset = _from_api_beatmapset(beatmapset)
+                session.merge(dbset)
+                for beatmap in beatmapset.beatmaps:
+                    session.merge(_from_api_beatmap(beatmap))
+                session.commit()
+                return dbset
+            except:
+                logger.exception(f"Failed to get beatmapset {beatmapset_id}")
+                return None
+            
---
+++ utils.py
@@ -0,0 +1,2 @@
+
+OSSAPI_GAMEMODES = {'osu': 0, 'taiko': 1, 'fruits': 2, 'mania': 3}
