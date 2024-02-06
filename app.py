55b0b46fb46594c050c0a8957507e304739ec56b
kanaarima <137962405+kanaarima@users.noreply.github.com>
2024-02-06 04:35:40 +0100

    Add event handler

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from common.database.postgres import Postgres
+from common.events import EventHandler
 from ossapi import Ossapi
 from config import Config
 
@@ -7,4 +8,4 @@ STOPPED = False
 config = Config()
 database = Postgres(config.postgres_user, config.postgres_password, config.postgres_host, config.postgres_port)
 ossapi = Ossapi(config.ossapi_id, config.ossapi_secret)
-
+events = EventHandler()
---
+++ events.py
@@ -0,0 +1,24 @@
+from typing import Type, Callable
+
+class EventHandler:
+    
+    def __init__(self) -> None:
+        self.handlers = {}
+
+    def add_handler(self, function: Callable, event: Type=None):
+        if event not in self.handlers:
+            self.handlers[event] = []
+        self.handlers[event].append(function)
+    
+    def remove_handler(self, function: Callable, event: Type=None):
+        if event not in self.handlers or function not in self.handlers[event]:
+            return
+        self.handlers[event].remove(function)
+    
+    def trigger(self, event: object):
+        for handler in self.handlers[None] if None in self.handlers else []:
+            handler(event)
+        if type(event) not in self.handlers:
+            return
+        for handler in self.handlers[type(event)]:
+            handler(event)
