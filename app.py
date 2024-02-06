a0f45425f5636ac422204d52c840dafa367eed8b
kanaarima <137962405+kanaarima@users.noreply.github.com>
2024-02-06 11:15:41 +0100

    disable ossapi debug logs

--- app.py
+++ app.py
@@ -3,9 +3,12 @@ from common.events import EventHandler
 from ossapi import Ossapi
 from config import Config
 
+import logging
+
 STOPPED = False
 
 config = Config()
 database = Postgres(config.postgres_user, config.postgres_password, config.postgres_host, config.postgres_port)
 ossapi = Ossapi(config.ossapi_id, config.ossapi_secret)
 events = EventHandler()
+ossapi.log.level = logging.ERROR
